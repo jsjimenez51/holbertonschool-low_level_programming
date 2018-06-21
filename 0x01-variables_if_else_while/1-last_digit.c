907ab27891964dc3afc255132462244cd10e2c75
jsjimenez51 <jsjimenez51@gmail.com>
2018-06-21 21:41:56 +0000

    Altered style to match Betty format

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+
 /**
  * main - assigns random value to n then prints
  * last number
@@ -14,10 +15,8 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
-	lastDigit = n % 10;
-
-	printf("Last digit of %d is %d ", n, lastDigit);
+	last_digit = n % 10;
+	printf("Last digit of %d is %d ", n, last_digit);
 	if (lastDigit > 5)
 		printf(" and is greater than 5\n");
 	else if (n == 0)
