964e83ef72382369c031291a2119a9d95dbdc273
jsjimenez51 <jsjimenez51@gmail.com>
2018-06-21 21:30:25 +0000

    Removed brackets from within block, then declared int above block

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,26 +10,19 @@
 int main(void)
 {
 	int n;
+	int lastDigit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	int lastDigit;
-
 	lastDigit = n % 10;
 
 	printf("Last digit of %d is ", n);
-	if (n > 5)
-	{
+	if (lastDigit > 5)
 		printf("%d and is greater than 5\n", lastDigit);
-	}
 	else if (n == 0)
-	{
 		printf("%d and is 0\n", n);
-	}
 	else
-	{
 		printf("%d and is less than 6 and not 0\n", lastDigit);
-	}
 	return (0);
 }
