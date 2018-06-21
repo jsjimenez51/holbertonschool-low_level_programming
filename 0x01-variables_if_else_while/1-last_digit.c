d44bf6554f907966f544e3ff462b55f5f581573f
jsjimenez51 <jsjimenez51@gmail.com>
2018-06-21 21:37:59 +0000

    Reformatted first printf line and took int out of if else loop

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,12 +17,12 @@ int main(void)
 
 	lastDigit = n % 10;
 
-	printf("Last digit of %d is ", n);
+	printf("Last digit of %d is %d ", n, lastDigit);
 	if (lastDigit > 5)
-		printf("%d and is greater than 5\n", lastDigit);
+		printf(" and is greater than 5\n");
 	else if (n == 0)
-		printf("%d and is 0\n", n);
+		printf(" and is 0\n");
 	else
-		printf("%d and is less than 6 and not 0\n", lastDigit);
+		printf(" and is less than 6 and not 0\n");
 	return (0);
 }
