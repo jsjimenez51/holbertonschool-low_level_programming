ce4af74a03a53993a6f741cb5569e48a23d4fff6
jsjimenez51 <jsjimenez51@gmail.com>
2018-06-21 00:05:02 +0000

    Inserted int into Size of long string

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,11 +7,11 @@ int main(void)
 	long long int longlongType;
 	float floatType;
 
-	printf("Size of a char: %ld byte'(s)'\n", sizeof(charType));
-	printf("Size of a int: %ld byte'(s)'\n", sizeof(intType));
-	printf("Size of a long: %ld byte'(s)'\n", sizeof(longType));
-	printf("Size of a long long int: %ld byte'(s)'\n", sizeof(longlongType));
-	printf("Size of a float: %ld byte'(s)'\n", sizeof(floatType));
+	printf("Size of a char: %ld byte(s)\n", sizeof(charType));
+	printf("Size of a int: %ld byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(longType));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(longlongType));
+	printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
 
 	return (0);
 }
