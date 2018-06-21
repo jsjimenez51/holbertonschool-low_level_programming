c6f9a7ddc4812530f3d1df582a7d515bbef0c56e
jsjimenez51 <jsjimenez51@gmail.com>
2018-06-21 00:12:11 +0000

    Removed declarations of integers and changed to %lu

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
 int main(void)
 {
-	char charType;
-	int intType;
-	long int longType;
-	long long int longlongType;
-	float floatType;
-
-	printf("Size of a char: %ld byte(s)\n", sizeof(charType));
-	printf("Size of a int: %ld byte(s)\n", sizeof(intType));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(longType));
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(longlongType));
-	printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of a int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 
 	return (0);
 }
