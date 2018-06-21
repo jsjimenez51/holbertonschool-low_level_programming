75beb182457809272679c02be43c052d703a1e8d
jsjimenez51 <jsjimenez51@gmail.com>
2018-06-21 00:00:06 +0000

    Create file 6-size.c
    Write a C program that prints the size of various types on the
    computer it is compiled and run on.

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void)
+{
+	char charType;
+	int intType;
+	long int longType;
+	long long int longlongType;
+	float floatType;
+
+	printf("Size of a char: %ld byte'(s)'\n", sizeof(charType));
+	printf("Size of a int: %ld byte'(s)'\n", sizeof(intType));
+	printf("Size of a long: %ld byte'(s)'\n", sizeof(longType));
+	printf("Size of a long long int: %ld byte'(s)'\n", sizeof(longlongType));
+	printf("Size of a float: %ld byte'(s)'\n", sizeof(floatType));
+
+	return (0);
+}
