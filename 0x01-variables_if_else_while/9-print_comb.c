76eca41e190d93dc0c7397ccc4e85895f9991504
jsjimenez51 <jsjimenez51@gmail.com>
2018-06-22 02:06:19 +0000

    Create file 9-print_comb.c
    Prints all possible combinations of single-digit numbers.

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combinations of single digits
+ *
+ *
+ * Return: (0);
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{	putchar(i);
+
+		if (i != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
