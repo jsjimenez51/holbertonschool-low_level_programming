362409406ca58f8c3cda68c6a6a9ec494a08746f
jsjimenez51 <jsjimenez51@gmail.com>
2018-06-26 17:34:29 +0000

    Create file 2-print_alphabet_x10.c
    Function tht prints 10 times the alphabet, in lowercase.

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - print a-z x10
+ *
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char  ch;
+	char i;
+
+	for (i = 0 ; i <= 10 ; i++)
+	{
+		for (ch = 'a' ; ch <= 'z' ; ch++)
+			_putchar(ch);
+		_putchar('\n');
+	}
+}
