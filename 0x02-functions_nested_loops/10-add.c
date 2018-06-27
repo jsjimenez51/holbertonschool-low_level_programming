ba729939e782bf80d8881d789a232a3a053a6192
jsjimenez51 <jsjimenez51@gmail.com>
2018-06-27 03:05:22 +0000

    Create file 10-add and updates to header file holberton.h

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * add - add two integers and returns result
+ * @a: 1st inputed number
+ * @b: 2nd inputed number
+ *
+ * Return: (n + '0') result
+ */
+int add(int a, int b)
+{
+	int n;
+
+	n = a + b;
+
+	return (n);
+}
