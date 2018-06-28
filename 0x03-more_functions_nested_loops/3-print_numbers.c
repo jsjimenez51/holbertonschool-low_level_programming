238e5ed58c38ff5720738e1dfa8d308e0e991cd2
Joshua Jimenez <jsjimenez51@gmail.com>
2018-06-28 22:43:29 +0000

    Create file 3-print_numbers.c and update header with prototype
    Function that prints numbers from 0 to 9 using prototype print_numbers

---
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9 then \n
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	char num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		_putchar(num);
+	}
+	_putchar('\n');
+}
--- 0x03-more_functions_nested_loops/holberton.h
+++ 0x03-more_functions_nested_loops/holberton.h
@@ -29,4 +29,6 @@ int _isdigit(int c);
 
 int mul(int a, int b);
 
+void print_numbers(void);
+
 #endif
