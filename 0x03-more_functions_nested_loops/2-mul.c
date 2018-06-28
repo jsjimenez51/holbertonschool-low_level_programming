a27ba9f5cbd074a886a2b04901f64273d2295bf5
Joshua Jimenez <jsjimenez51@gmail.com>
2018-06-28 22:31:08 +0000

    Create file 2-mul.c and update header for prototype
    Function that multiplies two integers, prototype int mul(int a int b)
    made

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: First inputted integer
+ * @b: Second inputted integer
+ *
+ * Return: the product of the two integers
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
