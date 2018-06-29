b5975d675f87f4fdbb1090f2d5fe92fe62171e89
Joshua Jimenez <jsjimenez51@gmail.com>
2018-06-29 02:00:36 +0000

    Create file 9-fizz_buzz.c and update header with prototype

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints 1 to 100 with buzz words
+ *
+ * Return: (0) Success
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 1 ; num <= 100 ; num++)
+	{
+		if (num % 15 == 0)
+			printf("FizzBuzz ");
+		else if (num % 3 == 0)
+			printf("Fizz ");
+		else if (num % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", num);
+	}
+	putchar('\n');
+	return (0);
+}
