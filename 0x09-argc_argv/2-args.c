2e1da7bad7496a92c96af152f8002094a66ff54e
Joshua Jimenez <jsjimenez51@gmail.com>
2018-07-16 06:09:36 +0000

    Create file 2-args.c
    Program that prints all arguments it receives.

---
+++ 0x09-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments received.
+ * @argc: number of arguments passed to the program.
+ * @argv: arguments passed to the program
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0 ; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
