7180dc22d01d385f18d2a9c6b77a8588ffefba8a
Joshua Jimenez <jsjimenez51@gmail.com>
2018-07-24 01:31:16 +0000

    Added new prototype to dog.h

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -15,6 +15,10 @@ struct dog
 	char *owner;
 };
 
+int _putchar(char c);
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
 
 #endif /* _DOG_H_ */
