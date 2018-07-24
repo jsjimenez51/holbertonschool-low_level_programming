75f150b6c21bd37cc5d194fabcc5a851d5fc6215
Joshua Jimenez <jsjimenez51@gmail.com>
2018-07-24 00:24:40 +0000

    Inserted descriptions for function under header

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -1,11 +1,18 @@
 #ifndef _DOG_H_
 #define _DOG_H_
 
+/**
+ * struct dog - groups input data for dog name, age, and owner
+ * @name: input dog's name
+ * @age: input dog's age
+ * @owner: input dog's owner
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog;
+};
 
 #endif /* _DOG_H_ */
