3994a765143642845aa6d023ab48244f4c64ffd2
Joshua Jimenez <jsjimenez51@gmail.com>
2018-07-25 22:02:34 +0000

    Corrected index error on line 8 and added stdlib to header

--- 0x0E-function_pointers/1-array_iterator.c
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -16,5 +16,5 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array && size && action)
 		for (ct = 0 ; ct < size ; ct++)
-			action(array[i]);
+			action(array[ct]);
 }
