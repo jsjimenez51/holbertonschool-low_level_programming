d89d40e2e630a922a29f5f08ffca074dad2d83a1
Joshua Jimenez <jsjimenez51@gmail.com>
2018-07-25 21:54:54 +0000

    Create file 1-array_iterator.c and update function_pointers.h header.
    File contains a function that executes a function given as a
    parameter on each element of an array. Prototype add to header:
    array_iterator.

---
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * array_iterator - executes a function given as a parameter on each element
+ * of an array.
+ * @array: pointer to the input array.
+ * @size: size of the input array.
+ * @action: pointer to the function to be used.
+ *
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t ct;
+
+	if (array && size && action)
+		for (ct = 0 ; ct < size ; ct++)
+			action(array[i]);
+}
