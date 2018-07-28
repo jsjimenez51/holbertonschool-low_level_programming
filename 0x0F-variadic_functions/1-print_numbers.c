5818587b0a2cc579b29d75f7612cb2c8c831676c
Joshua Jimenez <jsjimenez51@gmail.com>
2018-07-28 18:27:54 +0000

    Create file 1-print_numbers.c and update prototype in header
    File contains a function that prints numbers, followed by a new line.
    Prototype added to the header variadic_functions.h : print_numbers.

---
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - print input numbers followed by a new line.
+ * @separator: input string to be printed between numbers.
+ * @n: number of integers passed to the function.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list input;
+	unsigned int ct;
+
+	va_start(input, n);
+	for (ct = 0 ; ct < n ; ct++)
+	{
+		printf("%d", va_arg(input, unsigned int));
+		if ((ct < n -1) && separator)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(input);
+}
