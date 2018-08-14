745c0419b2a54445fdc2da264736ec7835bdea09
Joshua Jimenez <jsjimenez51@gmail.com>
2018-08-14 00:12:29 +0000

    Create file 1-print_binary.c and update prototype in header.
    Function prints the binary representation of a number. Prototype
    added to header: print_binary.

---
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_binary - prints the binary representation of a number.
+ * @n: number to have binary rep printed.
+ *
+ * Return: void.
+ */
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	if (n == 1)
+	{
+		_putchar('1');
+		return;
+	}
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar ((n & 1) + '0');
+}
