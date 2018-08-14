d463dc5948636c24070bb5173ceff9af4ae4ccbf
Joshua Jimenez <jsjimenez51@gmail.com>
2018-08-14 20:18:12 +0000

    Create file 1-create_file.c and update prototype in header.
    File contains a function that creates a file. Prototype added to
    header: create_file.

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * create_file - creates a file.
+ * @filename: file name to be used for file.
+ * @text_content: a NULL terminated string to write to the file.
+ *
+ * Return: 1 on succes, -1 on failure or filename is NULL.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, scribe, ct;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+
+	if (!fd)
+		return (-1);
+	if (!text_content)
+		return (1);
+
+	while (text_content[ct])
+		ct++;
+	scribe = write(fd, text_content, ct);
+	if (scribe != ct || scribe == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
