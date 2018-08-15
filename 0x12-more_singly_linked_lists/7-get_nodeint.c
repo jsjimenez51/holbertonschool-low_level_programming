ededec1b196156dd6dc191e7d9ddde4acb3dc832
Joshua Jimenez <jsjimenez51@gmail.com>
2018-08-15 20:25:46 +0000

    Create file 7-get_nodeint.c and update prototype in header.
    Function in file returns the nth node of a listint_t linked list.
    Prototype added to the header: get_nodeint_at_index.

---
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - returns the nth node of a listint_t linked list.
+ * @head: pointer to the head node.
+ * @index: index of the node, starting at 0.
+ *
+ * Return: NULL if the node does not exist, else returns the nth node.
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *get;
+	unsigned int ct;
+
+	if (head)
+	{
+		get = head;
+		while (get)
+		{
+			if (ct == index)
+				return (get);
+			get = get->next;
+			ct++;
+		}
+	}
+	return (NULL);
+}
