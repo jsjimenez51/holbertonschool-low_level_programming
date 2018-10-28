e2cec5d6eacb90fc3cbbff4a1d66dd98bc7af514
Joshua Jimenez <jsjimenez51@gmail.com>
2018-10-28 22:23:07 +0000

    UPDATE: 0-hash_table_create.c refactored code line 17

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -14,7 +14,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_ht = malloc(sizeof(hash_table_t));
 	if (new_ht == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 	new_ht->size = size;
 	new_ht->array = malloc(sizeof(hash_node_t *) * size);
