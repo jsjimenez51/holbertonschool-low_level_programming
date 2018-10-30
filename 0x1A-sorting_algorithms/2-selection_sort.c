6221de683857e0cf98a7a680ad814c2c93f8d2ee
Joshua S. Jimenez <Jsjimenez51@gmail.com>
2018-10-30 16:24:47 -0700

    CREATED: file 2-selection_sort.c

--- 0x1A-sorting_algorithms/2-selection_sort.c
+++ 0x1A-sorting_algorithms/2-selection_sort.c
@@ -8,3 +8,31 @@
  * Return: void
  */
 void selection_sort(int *array, size_t size)
+{
+	int i = 0;
+	int j = 0;
+	int target, ptr, lowest, temp;
+
+	for (; i < size -1 ; i++)
+	{
+		target = array[i];
+		ptr = array[i +1];
+
+		if (target < ptr)
+			lowest = target;
+		else
+			lowest = ptr;
+		for (; j < size ; j++)
+		{
+			if (ptr < lowest)
+				lowest = ptr;
+		}
+		if target < array[i + 1]
+			break;
+		else
+			temp = target;
+			target = lowest;
+			ptr = temp;
+	}
+
+}
