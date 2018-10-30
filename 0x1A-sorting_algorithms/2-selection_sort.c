c6e21d5f82d58c86f924609df51f092b78fadfc2
Joshua Jimenez <jsjimenez51@gmail.com>
2018-10-30 21:46:28 +0000

    CREATED: file 2-selection_sort.c

---
+++ 0x1A-sorting_algorithms/2-selection_sort.c
@@ -0,0 +1,10 @@
+#include "sort.h"
+
+/**
+ * selection_sort - function that executes a selection sort
+ * @array: pointer to the array to be sorted.
+ * @size: the size of the array
+ *
+ * Return: void
+ */
+void selection_sort(int *array, size_t size)
