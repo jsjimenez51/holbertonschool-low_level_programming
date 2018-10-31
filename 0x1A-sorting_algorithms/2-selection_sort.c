a0e9e134d1c8e03fc4561914c6c30a4b8fcc5e6c
Joshua Jimenez <jsjimenez51@gmail.com>
2018-10-31 23:50:37 +0000

    Refactored code

--- 0x1A-sorting_algorithms/2-selection_sort.c
+++ 0x1A-sorting_algorithms/2-selection_sort.c
@@ -3,47 +3,32 @@
 /**
  * selection_sort - function that executes a selection sort
  * @array: pointer to the array to be sorted.
- * @size: the size of the array
+ * @size: the size of the array.
  *
  * Return: void
  */
 void selection_sort(int *array, size_t size)
 {
-	unsigned int i = 0;
-	unsigned int j = 1;
-	int lowest, count, temp = 0;
+	unsigned int i, j;
+	unsigned int lowest;
+	int temp;
 
-	if (!array || size <2)
-		return;
-
-	for (; i < size - 1 ; i++)
+	for (i = 0 ; i < size - 1 ; i++)
 	{
-		count = 0;
-		j = i + 1;
-		if (array[i] < array[j])
+		lowest = i;
+		for (j = i + 1 ; j < size ; j++)
 		{
-			lowest = array[i];
-			count = i;
-		}
-		else
-		{
-			lowest = array[j];
-			count = j;
-		}
-
-		for (; j < size ; j++)
-		{
-			if (array[j] < lowest)
+			if (array[j] < array[lowest])
 			{
-				lowest = array[j];
-				count = j;
+				lowest = j;
 			}
 		}
-
-		temp = array[i];
-		array[i] = lowest;
-		array[count] = temp;
-		print_array(array, size);
+		if (lowest != i)
+		{
+			temp = array[lowest];
+			array[lowest] = array[i];
+			array[i] = temp;
+			print_array(array, size);
+		}
 	}
-
 }
