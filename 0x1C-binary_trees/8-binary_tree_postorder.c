f7e21f71996146db795fe448bd292266ce27b49b
Joshua Jimenez <jsjimenez51@gmail.com>
2018-12-12 22:01:04 +0000

    UPDATE: betty fixes

--- 0x1C-binary_trees/8-binary_tree_postorder.c
+++ 0x1C-binary_trees/8-binary_tree_postorder.c
@@ -7,11 +7,10 @@
 *
 * Return: void.
 */
-void binary_tree_postorder(const binary_tree_t *tree, void(*func)(int))
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (!tree || !func)
 		return;
-
 
 	binary_tree_postorder(tree->left, func);
 	binary_tree_postorder(tree->right, func);
